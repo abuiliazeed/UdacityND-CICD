e3333661aa56c5c94a9db5cef4159b5b11283b66
abuiliazeed <abuiliazeed@gmail.com>
2022-04-28 00:52:38 +0200

    successfully deployed

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: "Archive",
+  apiHost: "http://archive.eba-cvgjgpym.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
