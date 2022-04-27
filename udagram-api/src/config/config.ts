9b20f6fcf64c4397866938342de14f873a371e70
abuiliazeed <abuiliazeed@gmail.com>
2022-04-27 22:26:52 +0200

    closes #3

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -12,6 +12,7 @@ export const config = {
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
+  // we created S3 bucket in AWS
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
